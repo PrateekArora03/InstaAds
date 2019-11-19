af95ef9289e7c97db588f7ac2f4ccef361450bfb
prarora98 <prateekisthebest98@gmail.com>
2019-11-19 21:41:53 +0530

    done with post component

--- Front-End/src/components/layout/post/PostItem.js
+++ Front-End/src/components/layout/post/PostItem.js
@@ -33,6 +33,7 @@ function PostItem() {
                 <path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
               </g>
             </svg>
+            <span className="like-count">132 likes</span>
           </span>
           <span className="share-section">
             <svg viewBox="0 0 24 24" className="share-svg">
@@ -41,6 +42,13 @@ function PostItem() {
                 <path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"></path>
               </g>
             </svg>
+          </span>
+        </div>
+        <div className="description-section">
+          <span className="description-author">Prateek</span>
+          <span className="description-data">
+            Top 7 resources to learn python!Top 7 resources to learn python!Top
+            7 resources to learn python!Top 7 resources to learn python!
           </span>
         </div>
       </footer>
