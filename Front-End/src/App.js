5a6715dbefa681dd73a28382bda52ad4722ffc5e
Ckbhatia <chetansain86@gmail.com>
2019-11-21 14:52:27 +0530

    feat: add fetchUser functionality

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -3,17 +3,51 @@ import Header from "./components/layout/header/Header";
 import Post from "./components/layout/post/PostItem";
 import Profile from "./components/layout/profile/Profile";
 import PostItem from "./components/layout/post/PostItem";
-function App() {
-  return (
-    <div className="App">
-      {
-        <>
-          <Header />
-          {/* <PostItem /> */}
-        </>
-      }
-    </div>
-  );
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    if (localStorage.authToken) {
+      this.fetchUser(JSON.parse(localStorage.authToken));
+    }
+  }
+
+  fetchUser = async token => {
+    try {
+      const req = await axios.get("http://localhost:3000/api/user", {
+        headers: {
+          // TODO: Add dynamic token
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZGNmZjRmNTFhOTFlOTAwMjQ5MzIzYzciLCJpYXQiOjE1NzQzMjczOTAsImV4cCI6MTU3NTUzNjk5MH0.Q-95CB7otdxXXL2i8XGseuajKCxtMX6J5GSGVrw44JA"
+        }
+      });
+
+      localStorage.setItem("authToken", JSON.stringify(token));
+      // Update the state
+      this.setState({ user: req.data.user });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {
+          <>
+            {/* <Header /> */}
+            {/* <PostItem /> */}
+            {/* <Profile /> */}
+            {/* <Home /> */}
+          </>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
