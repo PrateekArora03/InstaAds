29567732115b5f67a24c279d1eb5aa64949faba2
Ckbhatia <chetansain86@gmail.com>
2019-11-27 15:07:42 +0530

    fix: remove admin status from login route

--- Back-End/routes/users.js
+++ Back-End/routes/users.js
@@ -38,7 +38,7 @@ router.post("/login", (req, res) => {
       return res.status(200).json({
         status: "success",
         message: "Admin logged in",
-        user: { authToken: token, isAdmin: true }
+        authToken: token
       });
     }
     // User request
@@ -46,7 +46,7 @@ router.post("/login", (req, res) => {
       return res.status(200).json({
         status: "success",
         message: "User logged in",
-        user: { authToken: token, isAdmin: false }
+        authToken: token
       });
     }
   });
