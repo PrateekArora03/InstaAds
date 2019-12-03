eb787ac1e9d2104c09181e37e54e8eaaf7e7b63d
prateekcodes <prateekisthebest98@gmail.com>
2019-12-03 23:08:29 +0530

    add: post upload file

---
+++ Front-End/src/components/layout/post/PostUpload.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+import { Input, Button, message, Upload, Icon } from "antd";
+
+import "./postupload.scss";
+
+const { TextArea } = Input;
+
+class PostUpload extends Component {
+  state = {
+    loading: false
+  };
+
+  enterLoading = () => {
+    this.setState({ loading: true });
+  };
+
+  render() {
+    const props = {
+      name: "file",
+      action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+      headers: {
+        authorization: "authorization-text"
+      },
+      onChange(info) {
+        if (info.file.status !== "uploading") {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === "done") {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === "error") {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      }
+    };
+    return (
+      <form className="post-upload-form">
+        <div className="input-post">
+          <TextArea rows={4} value="hello" />
+          <Upload {...props}>
+            <Button>
+              <Icon type="upload" /> Image
+            </Button>
+          </Upload>
+        </div>
+        <Button
+          type="primary"
+          loading={this.state.loading}
+          onClick={this.enterLoading}
+        >
+          Post
+        </Button>
+      </form>
+    );
+  }
+}
+export default PostUpload;
