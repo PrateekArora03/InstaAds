8d157378fe84ba4f004268b52da9bf22ea3a639d
prateekcodes <prateekisthebest98@gmail.com>
2019-12-05 17:28:05 +0530

    add: dashboard

--- Front-End/src/components/admin/dashboard.js
+++ Front-End/src/components/admin/dashboard.js
@@ -1,80 +1,66 @@
 import React from "react";
-import axios from "axios";
 
-import { Table, Divider } from "antd";
+import { Table, Divider, Tag } from "antd";
 
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: text => <a>{text}</a>
-  },
-  {
-    title: "Post",
-    dataIndex: "post",
-    key: "post"
-  },
-  {
-    title: "Image",
-    dataIndex: "img",
-    key: "img"
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (text, record) => (
-      <span>
-        <a>Accept</a>
-        <Divider type="vertical" />
-        <a>Reject</a>
-      </span>
-    )
-  }
-];
+const { Column, ColumnGroup } = Table;
 
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"]
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"]
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    tags: ["cool", "teacher"]
-  }
-];
-
-class Dashboard extends React.Component {
+class DashBoard extends React.Component {
   state = {
-    posts: null
+    posts: []
   };
-  fetchPosts = async ({ authToken }) => {
-    const res = await axios.get("https://localhost:3000/api/admin/dashboard", {
-      headers: {
-        Authorization: authToken
-      }
-    });
-    this.setState({ posts: res.data.posts });
-  };
-
-  async componentDidMount() {
-    if (localStorage.user) {
-      this.fetchUser(JSON.parse(localStorage.user));
+  fetchPosts = async authToken => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/admin/dashboard", {
+        headers: {
+          Authorization: authToken
+        }
+      });
+      // Update the state
+      this.setState({ posts: res.data.posts });
+    } catch (err) {
+      console.error(err);
     }
+  };
+  componentDidMount = () => {
+    if (localStorage.authToken) {
+      this.fetchUser(JSON.parse(localStorage.authToken));
+    }
+  };
+  render() {
+    return (
+      <Table dataSource={posts}>
+        <ColumnGroup title="Name">
+          <Column title="First Name" dataIndex="firstName" key="firstName" />
+          <Column title="Last Name" dataIndex="lastName" key="lastName" />
+        </ColumnGroup>
+        <Column title="Age" dataIndex="age" key="age" />
+        <Column title="Address" dataIndex="address" key="address" />
+        <Column
+          title="Tags"
+          dataIndex="tags"
+          key="tags"
+          render={tags => (
+            <span>
+              {tags.map(tag => (
+                <Tag color="blue" key={tag}>
+                  {tag}
+                </Tag>
+              ))}
+            </span>
+          )}
+        />
+        <Column
+          title="Action"
+          key="action"
+          render={(text, record) => (
+            <span>
+              <a>Invite {record.lastName}</a>
+              <Divider type="vertical" />
+              <a>Delete</a>
+            </span>
+          )}
+        />
+      </Table>
+    );
   }
 }
-
-module.exports = <Table columns={columns} dataSource={data} />;
