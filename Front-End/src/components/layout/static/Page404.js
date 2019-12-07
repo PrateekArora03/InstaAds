54323abe2e10be551513a8e4840b281b337a5137
prateekcodes <prateekisthebest98@gmail.com>
2019-12-07 23:41:59 +0530

    update: page404

--- Front-End/src/components/layout/static/Page404.js
+++ Front-End/src/components/layout/static/Page404.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { Result, Button } from "antd";
 
 export default function PageNotFound() {
-  return <h1>Page Not Found</h1>;
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary">
+          <Link to="/">Back Home</Link>
+        </Button>
+      }
+    />
+  );
 }
