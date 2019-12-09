619cf18028828007d95e686a5fe960b4ac3bbebc
ckbhatia <chetansain86@gmail.com>
2019-12-09 14:49:19 +0530

    feat: add loader and style loader

---
+++ Front-End/src/components/layout/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.scss";
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <img className="loader-image" src="/Loader.png" alt="loader" />
+    </div>
+  );
+}
+
+export default Loader;
