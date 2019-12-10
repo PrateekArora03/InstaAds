7de9cf686976f3e8e5d86f39341431bd677df63b
ckbhatia <chetansain86@gmail.com>
2019-12-10 18:26:32 +0530

    feat: add a new get ads route for list ads

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const Post = require("../models/post");
-const Auth = require("../auth/auth");
-
-// Verify the token
-router.use(Auth.verToken);
+const AdPost = require("../models/adPost");
+// const Auth = require("../auth/auth");
 
 /* GET home page. */
 router.get("/", async (req, res) => {
@@ -13,6 +11,18 @@ router.get("/", async (req, res) => {
       .sort({ createdAt: -1 })
       .limit(10);
     res.json({ message: "Got the posts", status: true, posts });
+  } catch (error) {
+    res.status(401).json({ message: "There's an error", status: false });
+  }
+});
+
+// Get ad posts
+router.get("/ads", async (req, res) => {
+  try {
+    const adPosts = await AdPost.find({ isApprove: true })
+      .sort({ createdAt: -1 })
+      .limit(10);
+    res.json({ message: "Got the Ad posts", status: true, adPosts });
   } catch (error) {
     res.status(401).json({ message: "There's an error", status: false });
   }
