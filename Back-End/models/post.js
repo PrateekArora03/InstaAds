00b3cdfded8f2c9910627caa611da6cbe4c83a16
prateekcodes <prateekisthebest98@gmail.com>
2019-12-22 20:37:34 +0530

    fix: remove postview and comment schema link from post

--- Back-End/models/post.js
+++ Back-End/models/post.js
@@ -10,9 +10,7 @@ const postSchema = Schema(
     location: String,
     author: { type: Object, required: true },
     isApprove: { type: Boolean, default: false },
-    // comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
     like: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    // views: { type: Schema.Types.ObjectId, ref: "PostView" }
     views: { type: Number, default: 0 }
   },
   { timestamps: true }
