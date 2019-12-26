83aab3e8f2bbdd5a50552c7f0529210d28e14de7
prateekcodes <prateekisthebest98@gmail.com>
2019-12-26 22:51:25 +0530

    Add: new dashboard

--- Front-End/src/components/admin/dashboard.js
+++ Front-End/src/components/admin/dashboard.js
@@ -1,103 +1,32 @@
 import React from "react";
-import axios from "axios";
+import { Tabs } from "antd";
 
-import { Table, Divider, message } from "antd";
+import Post from "./Dashboard/Post";
+import Carousel from "./Dashboard/Carousel";
 
-import "./dashboard.scss";
+const { TabPane } = Tabs;
 
-const { Column } = Table;
-
-class DashBoard extends React.Component {
-  state = {
-    posts: []
-  };
-
-  fetchPosts = async authToken => {
-    try {
-      const res = await axios.get("http://localhost:3000/api/admin/dashboard", {
-        headers: {
-          Authorization: authToken
-        }
-      });
-      // Update the state
-      this.setState({ posts: res.data.posts });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  approvePost = async id => {
-    const token = JSON.parse(localStorage.getItem("authToken"));
-    try {
-      const res = await axios.patch(
-        "http://localhost:3000/api/admin/post/" + id,
-        null,
-        {
-          headers: {
-            Authorization: token
-          }
-        }
-      );
-      if (res.data.status === "success") {
-        this.fetchPosts(token);
-        message.success("approve success");
-      } else {
-        message.error("Failed to approve");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  deletePost = async id => {
-    const token = JSON.parse(localStorage.getItem("authToken"));
-    try {
-      const res = await axios.delete(`http://localhost:3000/api/post/${id}`, {
-        headers: {
-          Authorization: token
-        }
-      });
-      if (res.data.status === "success") {
-        this.fetchPosts(token);
-        message.success("delete success");
-      } else {
-        message.error("delete failed");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  componentDidMount = () => {
-    if (localStorage.authToken) {
-      this.fetchPosts(JSON.parse(localStorage.authToken));
-    }
-  };
-
-  render() {
-    return (
-      <Table className="table" rowKey="_id" dataSource={this.state.posts}>
-        <Column title="Author" dataIndex="author.name" key="author" />
-        <Column title="Description" dataIndex="description" key="description" />
-        <Column
-          title="Action"
-          key="action"
-          render={(text, postData) => (
-            <span>
-              <a
-                title="author.name"
-                onClick={() => this.approvePost(postData._id)}
-              >
-                Approve
-              </a>
-              <Divider type="vertical" />
-              <a onClick={() => this.deletePost(postData._id)}>Delete</a>
-            </span>
-          )}
-        />
-      </Table>
-    );
-  }
+function dashboard() {
+  return (
+    <div
+      style={{
+        width: "80vw",
+        margin: "0 auto"
+      }}
+    >
+      <Tabs defaultActiveKey="3">
+        <TabPane tab="Post approval" key="1">
+          <Post />
+        </TabPane>
+        <TabPane tab="Ads approval" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Carousel" key="3">
+          <Carousel />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
 }
 
-export default DashBoard;
+export default dashboard;
