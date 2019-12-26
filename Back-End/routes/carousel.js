f5f9fcf1b17f6137a8f51538234133303649bbad
prateekcodes <prateekisthebest98@gmail.com>
2019-12-26 22:51:02 +0530

    add: carousel route

---
+++ Back-End/routes/carousel.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express();
+const Ad = require("./../models/ad");
+const auth = require("./../auth/auth");
+
+//Home Banner route
+router.patch("/", auth.verToken, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const ads = await Ad.find({});
+      await Ad.findOneAndUpdate(
+        { _id: ads[0]._id },
+        { homeBanner: req.body },
+        { new: true }
+      );
+      return res
+        .status(201)
+        .json({
+          status: "success",
+          message: `carousel Update to ${req.body.toggle}`
+        });
+    } catch (error) {
+      return res.status(400).json({ status: false, error });
+    }
+  } else {
+    res.json({ status: false, message: "unauthorized User" });
+  }
+});
+
+//
+router.get("/", async (req, res) => {
+  try {
+    const ads = await Ad.find({});
+    console.log(ads[0]);
+    res.status(200).json({ status: "success", ads: ads[0].homeBanner });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error });
+  }
+});
+
+module.exports = router;
