ac4c3dabad729bd0d322b6414d38dc21c09e3d78
prateekcodes <prateekisthebest98@gmail.com>
2019-12-29 23:00:05 +0530

    feat: send index response

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -1,29 +1,24 @@
 var express = require("express");
 var router = express.Router();
+
 const Post = require("../models/post");
 const AdPost = require("../models/adPost");
-// const Auth = require("../auth/auth");
 
 /* GET home page. */
 router.get("/", async (req, res) => {
   try {
     const posts = await Post.find({ isApprove: true })
       .sort({ createdAt: -1 })
-      .limit(10);
-    res.json({ message: "Got the posts", status: true, posts });
-  } catch (error) {
-    res.status(401).json({ message: "There's an error", status: false });
-  }
-});
-
-// Get ad posts
-router.get("/ads", async (req, res) => {
-  try {
-    const adPosts = await AdPost.find({ isApprove: true })
+      .limit(20);
+    const ads = await AdPost.find({
+      isApprove: true,
+      expireDate: { $gte: Date.now() }
+    })
       .sort({ createdAt: -1 })
-      .limit(10);
-    res.json({ message: "Got the Ad posts", status: true, adPosts });
+      .limit(6);
+    res.json({ message: true, status: true, posts, ads });
   } catch (error) {
+    console.log(error);
     res.status(401).json({ message: "There's an error", status: false });
   }
 });
