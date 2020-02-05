a70af362c2340e79d6c570c87b32fc133b941ea1
prateekcodes <prateekisthebest98@gmail.com>
2020-02-05 17:51:08 +0530

    update: postbuild script for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/users");
@@ -40,5 +41,14 @@ app.use("/api/post", postRouter);
 app.use("/api/adpost", adPostRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/carousel", carouselRouter);
+
+//serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
